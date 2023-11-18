c6efdd01fca6360dfffb880aa04cb3008570e122
aziemulkyt <151395928+aziemulkyt@users.noreply.github.com>
2023-11-19 02:08:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,20 @@
 import streamlit as st
 import subprocess
-import threading
 
 # Function to run a command and stream the output
 def run_command(cmd):
-    st.write(f"Running: `{cmd}`\n")
-    process = subprocess.Popen(
-        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
-    )
-
-    # Stream the output
-    for line in iter(process.stdout.readline, ''):
-        st.write(line)
-    
-    process.wait()
-    st.write(f"---\nCommand exited with return code {process.returncode}")
-
-# Function to run the command in a separate thread
-def run_command_threaded(cmd):
     if cmd:
-        thread = threading.Thread(target=run_command, args=(cmd,))
-        thread.start()
+        process = subprocess.Popen(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+        )
+
+        # Collect and return the output
+        output = ""
+        for line in iter(process.stdout.readline, ''):
+            output += line
+
+        process.wait()
+        return output, process.returncode
 
 # Streamlit UI
 st.title("Run Shell Commands")
@@ -30,4 +24,6 @@ user_input = st.text_input("Enter a shell command")
 
 # Button to run the command
 if st.button('Run Command'):
-    run_command_threaded(user_input)
+    output, returncode = run_command(user_input)
+    st.text_area("Output", output, height=300)
+    st.write(f"Command exited with return code {returncode}")
