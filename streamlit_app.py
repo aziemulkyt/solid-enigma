7c6eb97144752328fc8e244ac06d2e6d3778af61
aziemulkyt <151395928+aziemulkyt@users.noreply.github.com>
2023-11-19 02:15:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import subprocess
+import time
 
-# Function to run a command and stream the output
+# Function to run a command and append output to the session state
 def run_command(cmd):
-    if cmd:
-        process = subprocess.Popen(
-            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
-        )
+    if 'output' not in st.session_state or not cmd:
+        st.session_state.output = ""
 
-        # Collect and return the output
-        output = ""
-        for line in iter(process.stdout.readline, ''):
-            output += line
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+    )
 
-        process.wait()
-        return output, process.returncode
+    # Read the output line by line and update session state
+    for line in iter(process.stdout.readline, ''):
+        st.session_state.output += line
+        time.sleep(0.1)  # Small delay to allow for UI updates
 
 # Streamlit UI
 st.title("Run Shell Commands")
@@ -24,6 +24,11 @@ user_input = st.text_input("Enter a shell command")
 
 # Button to run the command
 if st.button('Run Command'):
-    output, returncode = run_command(user_input)
-    st.text_area("Output", output, height=300)
-    st.write(f"Command exited with return code {returncode}")
+    run_command(user_input)
+
+# Display the output
+st.text_area("Output", st.session_state.output, height=300)
+
+# Rerun the app every few seconds to update the output
+st_autorefresh_interval = 1  # seconds
+st.experimental_rerun() if time.time() % st_autorefresh_interval < 0.1 else None
